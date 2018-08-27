bd2df5a4e07b9d79abfc18ddd5ce62bb2e625e74
东方名都 <361904805@qq.com>
2018-08-27 16:22:22 +0800

    适配调整

--- code/asset/script/common.js
+++ code/asset/script/common.js
@@ -1,76 +1,49 @@
-// (function (doc, win) {
-//       var docEl = doc.documentElement,
-//         resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-//         recalc = function() {
-//           var clientWidth = docEl.clientWidth;
-//           if (!clientWidth) {
-//             return;
-//           }
-//           docEl.style.fontSize = Math.min(640, Math.max(docEl.clientWidth, 320)) / 320 * 14 + 'px';
-//         };
+//designWidth:设计稿的实际宽度值，需要根据实际设置
+//maxWidth:制作稿的最大宽度值，需要根据实际设置
+//这段js的最后面有两个参数记得要设置，一个为设计稿实际宽度，一个为制作稿最大宽度，例如设计稿为750，最大宽度为750，则为(750,750)
+;(function(designWidth, maxWidth) {
+  var doc = document,
+  win = window,
+  docEl = doc.documentElement,
+  remStyle = document.createElement("style"),
+  tid;
 
-//       if (!doc.addEventListener) return;
-//       win.addEventListener(resizeEvt, recalc, false);
-//       doc.addEventListener('DOMContentLoaded', recalc, false);
-//     })(document, window);
+  function refreshRem() {
+    var width = docEl.getBoundingClientRect().width;
+    maxWidth = maxWidth || 540;
+    width>maxWidth && (width=maxWidth);
+    var rem = width * 100 / designWidth;
+    remStyle.innerHTML = 'html{font-size:' + rem + 'px;}';
+  }
 
-    var pixclPatio = 1 / window.devicePixelRatio;
-document.write('<meta name="viewport" content="width=device-width,initial-scale='+pixclPatio+',minimum-scale='+pixclPatio+',maximum-scale='+pixclPatio+',user-scalable=no" />');
-var html = document.getElementsByTagName('html')[0];
-var pageWidth = html.getBoundingClientRect().width;
-html.style.fontSize = pageWidth / 16 + 'px';
+  if (docEl.firstElementChild) {
+    docEl.firstElementChild.appendChild(remStyle);
+  } else {
+    var wrap = doc.createElement("div");
+    wrap.appendChild(remStyle);
+    doc.write(wrap.innerHTML);
+    wrap = null;
+  }
+  //要等 wiewport 设置好后才能执行 refreshRem，不然 refreshRem 会执行2次；
+  refreshRem();
 
-// ;(function(designWidth, maxWidth) {
-//   var doc = document,
-//     win = window;
-//   var docEl = doc.documentElement;
-//   var tid;
-//   var rootItem,rootStyle;
+  win.addEventListener("resize", function() {
+    clearTimeout(tid); //防止执行两次
+    tid = setTimeout(refreshRem, 300);
+  }, false);
 
-//   function refreshRem() {
-//     var width = docEl.getBoundingClientRect().width;
-//     if (!maxWidth) {
-//       maxWidth = 540;
-//     };
-//     if (width > maxWidth) {
-//       width = maxWidth;
-//     }
-//     //与淘宝做法不同，直接采用简单的rem换算方法1rem=100px
-//     var rem = width * 100 / designWidth;
-//     //兼容UC开始
-//     rootStyle="html{font-size:"+rem+'px !important}';
-//     rootItem = document.getElementById('rootsize') || document.createElement("style");
-//     if(!document.getElementById('rootsize')){
-//     document.getElementsByTagName("head")[0].appendChild(rootItem);
-//     rootItem.id='rootsize';
-//     }
-//     if(rootItem.styleSheet){
-//     rootItem.styleSheet.disabled||(rootItem.styleSheet.cssText=rootStyle)
-//     }else{
-//     try{rootItem.innerHTML=rootStyle}catch(f){rootItem.innerText=rootStyle}
-//     }
-//     //兼容UC结束
-//     docEl.style.fontSize = rem + "px";
-//   };
-//   refreshRem();
+  win.addEventListener("pageshow", function(e) {
+    if (e.persisted) { // 浏览器后退的时候重新计算
+      clearTimeout(tid);
+      tid = setTimeout(refreshRem, 300);
+    }
+  }, false);
 
-//   win.addEventListener("resize", function() {
-//     clearTimeout(tid); //防止执行两次
-//     tid = setTimeout(refreshRem, 300);
-//   }, false);
-
-//   win.addEventListener("pageshow", function(e) {
-//     if (e.persisted) { // 浏览器后退的时候重新计算
-//       clearTimeout(tid);
-//       tid = setTimeout(refreshRem, 300);
-//     }
-//   }, false);
-
-//   if (doc.readyState === "complete") {
-//     doc.body.style.fontSize = "16px";
-//   } else {
-//     doc.addEventListener("DOMContentLoaded", function(e) {
-//       doc.body.style.fontSize = "16px";
-//     }, false);
-//   }
-// })(720, 640);
+  if (doc.readyState === "complete") {
+    doc.body.style.fontSize = "15px";
+  } else {
+    doc.addEventListener("DOMContentLoaded", function(e) {
+      doc.body.style.fontSize = "15px";
+    }, false);
+  }
+})(720, 720);
